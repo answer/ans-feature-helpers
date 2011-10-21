745619df5a168170383ecf4678875fa8408d63d0
sakai shunsuke <sakai@ans-web.co.jp>
2011-10-21 15:40:48 +0900

    initial commit

---
+++ lib/ans-feature-helpers.rb
@@ -0,0 +1,9 @@
+require "ans-feature-helpers/version"
+
+module Ans
+  module Feature
+    module Helpers
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ans-feature-helpers/version.rb
@@ -0,0 +1,7 @@
+module Ans
+  module Feature
+    module Helpers
+      VERSION = "0.0.1"
+    end
+  end
+end

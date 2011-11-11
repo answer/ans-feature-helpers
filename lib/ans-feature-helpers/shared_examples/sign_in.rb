da993ba05f2626d7be2da0561cf1bed6511e8ce4
sakai shunsuke <sakai@ans-web.co.jp>
2011-11-11 17:06:46 +0900

    xhr 用 example の追加

--- lib/ans-feature-helpers/shared_examples/sign_in.rb
+++ lib/ans-feature-helpers/shared_examples/sign_in.rb
@@ -8,3 +8,21 @@ shared_examples_for "Ans::Feature::Helpers::SignIn" do
     end
   end
 end
+
+shared_examples_for "Ans::Feature::Helpers::SignIn(xhr)" do
+  context "にログインせずにアクセスした場合" do
+    it "は、 401 レスポンスコード" do
+      action_は
+      response.response_code.should == 401
+    end
+  end
+end
+
+shared_examples_for "Ans::Feature::Helpers::SignIn(xhr-destroy)" do
+  context "にログインせずにアクセスした場合" do
+    it "は、 302 レスポンスコード" do
+      action_は
+      response.response_code.should == 302
+    end
+  end
+end
